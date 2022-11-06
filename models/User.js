e55d5021fdf5adaf2d3299b78298648f7e6a06fd
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-06 11:10:16 +0100

    ✨ Connect and create DB: - create employee model - create user Model - create /config/dbConn.js - connect db in server.js

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	firstname: {
+		type: String,
+		required: true
+	},
+	lastname: {
+		type: String,
+		required: true
+	},
+	picture: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	company: {
+		type: String,
+		required: true
+	},
+	roles: {
+		User: {
+			type: Number,
+			default: 2001
+		},
+		Editor: Number,
+		Admin: Number
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	refreshToken: String
+})
+
+module.exports = mongoose.model('User', userSchema)
