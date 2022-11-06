6503871a0b96292ee21bf72ebbf2a07ed8b7d571
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-06 10:10:12 +0100

    ✨ Add middlewares: - create logger middleware - create errorHandler middleware - add json and urlencoded middleware - create config allowedOrigins.js (devMode => localHost) - add cors middleware and its config files - add cookieParser middleware

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const app = express()
 const PORT = process.env.PORT || 3500
+const {logger} = require('./middlewares/logger')
+const errorHandler = require('./middlewares/errorHandler')
+const cookieParser = require('cookie-parser')
 
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+app.use(cookieParser())
 app.use(express.static('public'))
 
 app.use('/', require('./routes/root'))
@@ -18,5 +29,7 @@ app.all('*', (req, res) => {
 		res.type('txt').send('404 Not Found')
 	}
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
