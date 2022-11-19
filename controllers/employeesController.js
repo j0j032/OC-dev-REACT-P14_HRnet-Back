08d441f16233fc5ed20db369633423e3ca5510f6
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-19 14:04:45 +0100

    🚧 update find method that takes the query param text to match the result ask in frontend

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,14 +1,29 @@
 const Employee = require('../models/Employee')
 
 const getAllEmployees = async (req, res) => {
-	const page = req.query.p < 0 || null ? 0 : req.query.p || 0
-	const employeesPerPage = req.query.limit || 12
+	if (req.query.limit === 'null' || req.query.limit === 'undefined' || req.query.limit === undefined)
+		return res.status(400).json({'message': 'Bad Request, the query param should be a number'})
+	
+	const page = req.query.page < 0 || null ? 0 : req.query.page || 0
+	const limit = req.query.limit === '0' ? 12 : req.query.limit || 12
+	const text = req.query.text || ''
+	//db.Employee.createIndex({'$**': 'text'})
+	
 	const employeesLength = await Employee.count()
+	
 	const employees = await Employee
-		.find()
+		.find({
+			$or: [
+				{title: {$regex: text, $options: 'i'}},
+				{department: {$regex: text, $options: 'i'}},
+				{firstname: {$regex: text, $options: 'i'}},
+				{lastname: {$regex: text, $options: 'i'}}
+			]
+		})
 		.sort({lastname: 1})
-		.skip(page * employeesPerPage)
-		.limit(employeesPerPage)
+		.skip(page * limit)
+		.limit(limit)
+	
 	if (!employees) return res.status(204).json({'message': 'No employees found.'})
 	res.json({employees, employeesLength})
 }
